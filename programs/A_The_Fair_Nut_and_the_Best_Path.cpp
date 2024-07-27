75cf39956d33d82e9e334fce104d351f228eea42
Mathavaroopan <roopanmathava@gmail.com>
2024-07-27 19:59:56 +0530

    added dp on trees

--- programs/A_The_Fair_Nut_and_the_Best_Path.cpp
+++ programs/A_The_Fair_Nut_and_the_Best_Path.cpp
@@ -10,16 +10,44 @@
 #define print(arr) for(int i : arr) cout << i << " "
 #define var(a) cout << #a << " = " << a << endl
 #define min(arr) *min_element(arr.begin(), arr.end())
-#define max(arr) *max_element(arr.begin(), arr.end())
 #define sum(arr) accumulate(arr.begin(), arr.end(), 0)
 #define endl '\n'
 using namespace std;
 
 const int mod = 1e9 + 7;
 int inf = 1e18;
+int n;
+vector<vector<pair<int, int>>> adj;
+vector<int> fuel;
+vector<vector<int>> dp;
+int ans = 0;
+void dfs(int node, int parent = -1){
+    dp[node][0] = dp[node][1] = fuel[node];
+    vector<int> temp;
+    for(auto it : adj[node]){
+        if(it.first == parent) continue;
+        dfs(it.first, node);
+        temp.push_back(dp[it.first][0] - it.second);
+    }
+    srtrev(temp);
+    if(!temp.empty()) dp[node][0] = max(dp[node][0], temp[0] + fuel[node]);
+    if(temp.size() > 1) dp[node][1] = max(dp[node][1], temp[0] + temp[1] + fuel[node]);
+    ans = max(ans, max(dp[node][0], dp[node][1]));
+}
 
 void solve(){
-    
+    cin >> n;
+    adj.assign(n + 1, vector<pair<int, int>>());
+    fuel.assign(n + 1, 0);
+    dp.assign(n + 1, vector<int>(2, 0));
+    for(int i = 1; i <= n; i++) cin >> fuel[i];
+    for(int i = 0; i < n - 1; i++){
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+    dfs(1);
+    cout << ans << endl;
 }
  
 int32_t main()
@@ -29,7 +57,7 @@ int32_t main()
     cin.tie(NULL);
  
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while (T--) solve();
     return 0;
 }
